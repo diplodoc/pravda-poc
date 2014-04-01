fe5ba4b29383e38034b881555b15a1a35c1f4708
yaroslav.yermilov <yermilov17@gmail.com>
2014-04-02 01:22:22 +0300

    DIPLO-4. load web page

--- src/main/groovy/diplodoc/poc/pravda/LoadPravdaLatestNews.groovy
+++ src/main/groovy/diplodoc/poc/pravda/LoadPravdaLatestNews.groovy
@@ -1,6 +1,17 @@
 package diplodoc.poc.pravda
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
 /**
  * @author yaroslav.yermilov
  */
-println "Hello, diplodoc!"
+def url = 'http://www.pravda.com.ua/news/'
+
+println "Loading '${url}'..."
+
+Document document = Jsoup.connect(url).get();
+println 'Loaded document:'
+println '================'
+println document
+println '================'
