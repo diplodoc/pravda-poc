d6672383d446b45675281b8d76c8ce7467709bbe
yaroslav.yermilov <yermilov17@gmail.com>
2014-04-02 01:33:46 +0300

    DIPLO-5. parse page to establish news list

--- src/main/groovy/diplodoc/poc/pravda/LoadPravdaLatestNews.groovy
+++ src/main/groovy/diplodoc/poc/pravda/LoadPravdaLatestNews.groovy
@@ -2,6 +2,7 @@ package diplodoc.poc.pravda
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 /**
  * @author yaroslav.yermilov
@@ -14,4 +15,20 @@ Document document = Jsoup.connect(url).get();
 println 'Loaded document:'
 println '================'
 println document
+println '================'
+
+println 'News elements:'
+println '================'
+Elements newsElements = document.select('dd')
+newsElements.each {
+    println """
+===
+Caption:     ${it.select('dd > a').text()}
+Description: ${it.select('dd > p > a').text()}
+Link:        ${it.select('dd > a').attr 'href'}
+===
+${it}
+===
+"""
+}
 println '================'
