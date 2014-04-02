25681fe2941fc6bf3056d9f9bd23dd421102f33b
yaroslav.yermilov <yermilov17@gmail.com>
2014-04-03 01:21:20 +0300

    DIPLO-6. separate NewsEntity class

--- src/main/groovy/diplodoc/poc/pravda/LoadPravdaLatestNews.groovy
+++ src/main/groovy/diplodoc/poc/pravda/LoadPravdaLatestNews.groovy
@@ -1,8 +1,6 @@
 package diplodoc.poc.pravda
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 
 /**
  * @author yaroslav.yermilov
@@ -12,22 +10,29 @@ def url = 'http://www.pravda.com.ua/news/'
 println "Loading '${url}'..."
 
 def document = Jsoup.connect(url).get();
-println 'Loaded document:'
-println '================'
-println document
-println '================'
+
+def newsEntities = document.select('dd').collect {
+    def caption = it.select('dd > a').text()
+
+    def description = it.select('dd > p > a').text()
+
+    def link = it.select('dd > a').attr('href')
+    link = (link.startsWith('http') ? link : "http://www.pravda.com.ua${link}")
+
+    new NewsEntity(
+        caption: caption,
+        description: description,
+        link: link
+    )
+}
 
 println 'News elements:'
 println '================'
-document.select('dd').each {
+newsEntities.each {
     println """
-===
-Caption:     ${it.select('dd > a').text()}
-Description: ${it.select('dd > p > a').text()}
-Link:        ${it.select('dd > a').attr 'href'}
-===
-${it}
-===
+Caption:     ${it.caption}
+Description: ${it.description}
+Link:        ${it.link}
 """
 }
 println '================'
---
+++ src/main/groovy/diplodoc/poc/pravda/NewsEntity.groovy
@@ -0,0 +1,12 @@
+package diplodoc.poc.pravda
+
+import java.time.LocalDateTime
+
+/**
+ * @author yaroslav.yermilov
+ */
+class NewsEntity {
+    String caption
+    String description
+    String link
+}
